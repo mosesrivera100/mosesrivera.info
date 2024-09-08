47f07f4c2fd059e1fc00f87bb7db3b6d89457db4
mosesrivera100@ <mosesrivera100@gmail.com>
2024-09-08 08:44:14 -0500

    Render and update site

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+# Render the Quarto website
+quarto render
+
+# Add all changes to Git
+git add .
+
+# Commit changes with a message (you can modify the message)
+git commit -m "Render and update site"
+
+# Push the changes to GitHub
+git push
